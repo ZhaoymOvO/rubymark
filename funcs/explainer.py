3e24ce82f4a120f58bfec1f2f629205f7d76334c
RikaaaOwO <zhaoym233@hotmail.com>
2026-04-17 11:49:26 +0800

    func: add del block support

--- funcs/explainer.py
+++ funcs/explainer.py
@@ -16,7 +16,21 @@ def explain(markdownText: str) -> str:
     Returns:
         str: html text output
     """
-    result: str = markdown.markdown(markdownText, output_format="html")
+    result: str = markdown.markdown(
+        markdownText,
+        output_format="html",
+        extensions=[
+            "extra",
+            "tables",
+            "toc",
+            "fenced_code",
+            "nl2br",
+            "sane_lists",
+            "codehilite",
+            "meta",
+        ],
+    )
+
     rubyList: list[str] = re.findall(r"\{.+?\}\(.+?\)", result)
     logging.debug(rubyList)
     for ruby in rubyList:
@@ -24,6 +38,11 @@ def explain(markdownText: str) -> str:
         rubyBase: str = re.findall(r"\{(.+?)\}", ruby)[0]
         logging.debug(f"rubyText: {rubyText}; rubyBase: {rubyBase}")
         result = result.replace(
-            ruby, f"<ruby>{rubyText}<rp> (</rp><rt>{rubyBase}</rt><rp>) </rp></ruby>"
+            ruby, f"<ruby>{rubyBase}<rp> (</rp><rt>{rubyText}</rt><rp>) </rp></ruby>"
         )
+
+    delList: list[str] = re.findall(r"~~.+?~~", result)
+    for delBlock in delList:
+        result = result.replace(delBlock, f"<del>{delBlock[2:-2]}</del>")
+
     return result
